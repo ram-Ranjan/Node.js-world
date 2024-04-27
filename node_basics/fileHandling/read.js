fbc537b6b4b368b7f8505fb929cdab4fb8a9cc77
Ranjan Kumar <127039261+ram-Ranjan@users.noreply.github.com>
2024-04-27 09:27:03 +0000

    read operation completed

---
+++ node_basics/fileHandling/read.js
@@ -0,0 +1,6 @@
+const { readFileSync } = require('fs');
+const txt= readFileSync('./node_basics/fileHandling/hello.txt' , 'utf8');
+        console.log(txt)
+// });
+
+console.log('do this ASAP')
---
+++ node_basics/index.js
@@ -0,0 +1,22 @@
+
+//console.log("Hello World")
+            
+
+//events
+// process.on('exit',function(){
+
+// })
+
+// const { EventEmitter } = require('events');
+// const eventEmitter = new EventEmitter();
+// eventEmitter.on('lunch',()=>{
+//     console.log("yumm ")
+// })
+// eventEmitter.emit('lunch')
+// eventEmitter.emit('lunch')
+
+
+//Reading and Writing
+
+
+
