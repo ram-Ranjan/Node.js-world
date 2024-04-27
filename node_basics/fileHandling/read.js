4575bc59a2b4214f5bc820f9a3965098be529ee5
Ranjan Kumar <127039261+ram-Ranjan@users.noreply.github.com>
2024-04-27 10:34:10 +0000

    Node app package.json created and modules configured

--- node_basics/fileHandling/read.js
+++ node_basics/fileHandling/read.js
@@ -1,6 +1,17 @@
-const { readFileSync } = require('fs');
-const txt= readFileSync('./node_basics/fileHandling/hello.txt' , 'utf8');
-        console.log(txt)
-// });
+// const { readFileSync } = require('fs');
+// const txt= readFileSync('./node_basics/fileHandling/hello.txt' , 'utf8');
+//         console.log(txt)
+// // });
 
-console.log('do this ASAP')
+// console.log('do this ASAP');
+
+
+const { readFile } = require('fs').promises;
+
+hello()
+
+async function hello(){
+        
+        const file = await readFile('./node_basics/fileHandling/hello.txt' , 'utf8');
+        console.log(file)
+}
